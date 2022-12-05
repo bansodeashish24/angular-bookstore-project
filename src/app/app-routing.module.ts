b0c5d81d3995e759c30cf6f3c6d3f4b1d35597df
Ashish <bansodeashish24@gmail.com>
2022-12-05 16:52:04 +0530

    add app level components and page-not-found component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BookModule } from './book/book.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// localhost:4200
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'books',
+  },
+  {
+    path: 'books',
+    loadChildren: () => BookModule,
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
